54481ad4815f794f3b382799225ef36edb4ab2ca
imedvede <Ilia.Medvedev@t-systems.com>
2020-04-16 22:57:22 +0300

    adjust controller

--- employee/web/src/main/java/com/ilya/controller/MainController.java
+++ employee/web/src/main/java/com/ilya/controller/MainController.java
@@ -3,7 +3,10 @@ package com.ilya.controller;
 import com.ilya.DataService;
 import com.ilya.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by ilya on 04.04.2020.
@@ -20,23 +23,26 @@ public class MainController {
         return dataService.getAll();
     }
 
-    @RequestMapping(value = "/removeAll", method = RequestMethod.DELETE)
-    public void removeAll(){
+    @RequestMapping(value = "/removeAll", method = RequestMethod.GET)
+    public void removeAll(HttpServletResponse httpServletResponse){
         dataService.deleteAll();
+        httpServletResponse.setHeader("Location", "http://localhost:3000");
+        httpServletResponse.setStatus(302);
     }
 
     @RequestMapping(value = "/add",method = RequestMethod.POST)
-    public void addEmployee(@RequestBody Employee employee){
-        dataService.addEmployee(employee);
+    public Employee addEmployee(@RequestBody Employee employee){
+        return dataService.addEmployee(employee);
     }
 
-    @RequestMapping(value = "/change",method = RequestMethod.PUT)
-    public void changeSalary(@RequestBody Employee employee){
-        dataService.changeSalary(employee);
+    @RequestMapping(value = "/change",method = RequestMethod.POST)
+    public Employee changeSalary(@RequestBody Employee employee){
+        return dataService.changeSalary(employee);
     }
 
     @RequestMapping(value = "/test")
-    public void test(){
+    public void test(Authentication authentication){
         System.out.println("test");
+        System.out.println(authentication.getAuthorities().iterator().next().getAuthority());
     }
 }
